ccfa60c9d43ef9de41a23d761bbbd4e18f89fb66
FelipeTolentino <felipe.tolentino@sempreceub.com>
2025-07-29 20:35:23 -0300

    Alterações no componente Header, criação do componente Footer e Food

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,19 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
 
 function App() {
+   return (
+      // As chaves (fragmentadas) são usadas para exportar apenas um componente, esse podendo conter outros dentro dele
+      // pois só se pode exportar um componente. O componente pode também ter o fechamento implicito, ao invés do normal.
 
+      // A ordem que os componentes são importados importa.
+      <>
+         <Header />
+         <Food />
+         <Footer />
+      </>
+   );
 }
 
 export default App
