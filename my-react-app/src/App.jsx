7f7506b593798605e3d3d67c94bac29c4665788a
FelipeTolentino <felipe.tolentino@sempreceub.com>
2025-07-29 20:37:21 -0300

    Talvez tenha feito caca

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,17 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
 
 function App() {
+   return (
+      // As chaves (fragmentadas) são usadas para exportar apenas um componente, esse podendo conter outros dentro dele
+      // pois só se pode exportar um componente. O componente pode também ter o fechamento implicito, ao invés do normal.
 
-}
-
-export default App
+      // A ordem que os componentes são importados importa.
+      <>
+         <Header />
+         <Food />
+         <Footer />
+      </>
+   );
+t App
