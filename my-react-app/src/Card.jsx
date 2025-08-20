0216e038a268d2c16f8f4410d30b9bf230ce178e
FelipeTolentino <felipe.tolentino@sempreceub.com>
2025-08-19 21:44:35 -0300

    Foto colocada no card component
    Apenas adicionei a foto de perfil no card component

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,12 +1,13 @@
-// ADICIONAR A FOTO DE PERFIL, COLOCAR COMENTARIO ABAIXO NAS LIÇÕES APRENDIDAS
+// COLOCAR COISAS DO CHATGPT DE GIT NO NOTAS AUTOADESIVAS
 
+import profilePic from './assets/profile.png'
 
 function Card() {
    // Para criar uma classe, não podemos usar "class" pois é uma palavra reservada em JSX, ao invés disso
    // pode ser usado o "className"
    return (
       <div className="card">
-         <img alt="profile picture" src=""></img>
+         <img alt="profile picture" src={profilePic}></img>
          <h2>Felipe Tolentino</h2>
          <p>I'm currently studying Computer Science in my university UniCEUB. Specifically, right now, i'm studying React!</p>
       </div>
